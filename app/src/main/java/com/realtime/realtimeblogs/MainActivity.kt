bab911e2c8b46b0697ef9e27433ec06dae621a49
Shahzad Afridi <shahzadahmadafridi@gmail.com>
2022-06-18 14:27:12 +0500

    Resovled conflicts

--- app/src/main/java/com/realtime/realtimeblogs/MainActivity.kt
+++ app/src/main/java/com/realtime/realtimeblogs/MainActivity.kt
@@ -1,35 +1,15 @@
 package com.realtime.realtimeblogs
 
-import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposableTargetMarker
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.ExperimentalUnitApi
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
-import androidx.compose.ui.unit.dp
-import com.realtime.realtimeblogs.ui.theme.BorderColor
 import com.realtime.realtimeblogs.ui.theme.PostyTheme
-import com.realtime.realtimeblogs.ui.theme.RoundRectShape
 import com.realtime.realtimeblogs.ui.theme.items.PostCard
 
 class MainActivity : ComponentActivity() {
@@ -62,4 +42,5 @@ fun DefaultPreview() {
         PostCard()
     }
 }
+
 
